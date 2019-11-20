2b3115c0af517f1885896fb65385689fdb746cd8
brendan-sweeney-liberty-it <brendan.sweeney@liberty-it.co.uk>
2019-11-20 16:20:56 +0000

    6 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { WorkshopListComponent } from './workshop-list/workshop-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, WorkshopListComponent, WelcomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
