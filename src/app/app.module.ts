6e652ca7b1e82486829cfa41f37ec7be821641bf
brendan-sweeney-liberty-it <brendan.sweeney@liberty-it.co.uk>
2019-11-20 16:48:40 +0000

    5 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { DataTablesModule } from 'angular-datatables';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { WorkshopListComponent } from './workshop-list/workshop-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import {AppRoutingModule} from './app-routing.module';
+import { WorkshopService } from './workshop-list/workshop.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, DataTablesModule ],
   declarations: [ AppComponent, HelloComponent, WorkshopListComponent, WelcomeComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [WorkshopService]
 })
 export class AppModule { }
